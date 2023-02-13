1caa255fec53f929fc2f853d3c3750bc5b4cd9fd
Paola Quiroga <paolaquirogap10@gmail.com>
2023-02-13 10:25:38 -0500

    Se hacen algunos cambios en el diagrama, y se crea funcion para validar si una ruta existe

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
+const chalk = require("chalk");
 const { mdLinks } = require("./index.js");
 
+console.log(chalk.magenta('-------------HOLA BIENVENIDA----------------'))
 mdLinks("./routstesting")
   .then(() => {})
   .catch((error) => {
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { absolutePath } = require("./functions.js");
+const { relativePath } = require("./functions.js");
 const chalk = require("chalk");
 
 const mdLinks = (path, options) => {
   return new Promise((resolve, reject) => {
     // Identificar si la ruta exite
-    absolutePath()
+    relativePath()
       .then((respuesta) => {
         console.log(chalk.magenta(respuesta));
       })
