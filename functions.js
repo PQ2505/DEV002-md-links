877589527e43e87b47752a521aa7227e4d937fed
Paola Quiroga <paolaquirogap10@gmail.com>
2023-02-15 11:22:11 -0500

    Se hacen cambios en las funciones

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const chalk = require('chalk');
 
 // Función para validar si existe la ruta
 const existPath = (paths) => fs.existsSync(paths);
@@ -9,17 +10,25 @@ const absolutePath = (paths) => {
   return path.isAbsolute(paths) ? paths : path.resolve(paths); 
 };
 
-// Funcion para validar si el archivo es .md y su extención
+// Función para validar si el archivo es .md y su extención
 const existFile = (paths) => path.extname(paths) === '.md';
+
 
 //Función para validar si es un File (archivo)
 const validateFile = (paths) => fs.statSync(paths).isFile();
+console.log(chalk.magenta(validateFile('./routstesting/Test0.md')));
+
+// Función que lee un archivo .md
+const validateReadFileMd = (paths) => fs.readFileSync(paths, 'utf8');
+console.log(validateReadFileMd('./routstesting/Test1.md'));
 
 // Función para validar si es un directorio
 const validateDirectory =  (paths) => fs.statSync(paths).isDirectory();
+console.log(validateDirectory('./routstesting'));
 
-// 
-
+// Función para leer un directorio
+const readingDirectory =(paths) => fs.readdirSync(paths);
+console.log(readingDirectory('./routstesting'));
 
 
 module.exports = {
@@ -28,4 +37,6 @@ module.exports = {
   existFile,
   validateFile,
   validateDirectory,
+  readingDirectory,
+  validateReadFileMd,
 };
